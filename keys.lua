de6600a947fc495817c3128938596d2e006a76a9
Tom Hinton <tom.hinton@cse.org.uk>
2018-03-23 16:25:41 +0000

    add client menu

--- keys.lua
+++ keys.lua
@@ -18,6 +18,8 @@ function keys:define_global()
       key({M, C}, "r", awesome.restart, {description = "restart awesome", group = "session"}),
       key({M},    "q", function () awful.screen.focused().menu:toggle() end),
 
+      key({M},    "y", function () awful.menu.clients({ theme = {width = 500} }) end),
+
       key({M}, "h", util.narrow_master, {description = "narrow master", group = "layout"}),
       key({M}, "l", util.widen_master, {description = "widen master", group = "layout"}),
       key({M}, "x", util.run, {description = "prompt", group="spawn"}),
